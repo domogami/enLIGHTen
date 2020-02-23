bec008505f4b65b4ca75aa4091213e8299cd6e62
Dominick <d3lee@ucsd.edu>
2020-02-23 13:13:44 -0800

    Workathon 2 update

--- enLIGHTen/enLIGHTen/ContentView.swift
+++ enLIGHTen/enLIGHTen/ContentView.swift
@@ -19,10 +19,51 @@ struct ContentView: View {
    
         Group {
             if (session.session != nil) {
-                Text("Welcome back user!")
-                Button(action: session.signOut) {
-                    Text("Sign Out")
+                ZStack {
+                    VStack {
+                        Spacer()
+                    }
+                    
+                        
+                    VStack {
+                        HStack {
+                            Text("Active Switches")
+                                .font(.system(size: 28, weight: .bold))
+                            
+                            Spacer()
+                            
+                            Button(action: session.signOut) {
+                                Text("Sign Out")
+                            }
+                            
+                        }
+                        .padding(.horizontal, 24)
+                        .padding(.top, 30)
+                        
+                        Spacer()
+                    }
+                    VStack {
+                        HStack {
+                            Text("Switch 1")
+                            Image(systemName: "lightbulb")
+                                .font(.system(size: 24, weight: .semibold))
+                        }
+                        .padding(20)
+                        .background(Color.green)
+                    }
                 }
+                .padding(.top, 44)
+                .background(
+                        VStack {
+                            LinearGradient(gradient: Gradient(colors: [Color("background2"), Color.white]), startPoint: .top, endPoint: .bottom)
+                                .frame(height: 200)
+                            Spacer()
+                        }
+                        .background(Color.white)
+                    )
+                    .edgesIgnoringSafeArea(.all)
+                
+     
             } else {
                 AuthView()
             }

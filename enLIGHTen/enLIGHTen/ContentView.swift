4514efbe7d4066efb9f774af37c136c5f6fa1eb0
Dominick <d3lee@ucsd.edu>
2020-02-26 17:24:16 -0800

    scrollview and foreach

--- enLIGHTen/enLIGHTen/ContentView.swift
+++ enLIGHTen/enLIGHTen/ContentView.swift
@@ -16,27 +16,17 @@ struct ContentView: View {
     var messageRef: DatabaseReference!
     @State private var toggleSwitch: Bool = false
     @State private var toggleML: Bool = false
+    //var SwitchArray: Array = []
     
     func getUser () {
         session.listen()
     }
-    
-    
-//    let ref = Database.database().reference().child("Switch").child("Switch_status")
-//
-//    ref.observeSingleEvent(of: .childAdded, with: { (snapshot) in
-//         if let userDict = snapshot.value as? [String:Any]
-//    }
     
     var body: some View {
    
         Group {
             if (session.session != nil) {
                 ZStack {
-                    
-//                    if self.Ml1 {
-//                        //TODO
-//                    }
                     
                     Color.green
                         .edgesIgnoringSafeArea(.all)
@@ -80,83 +70,86 @@ struct ContentView: View {
                     .padding(.top, 70)
                     
                     
-                    // Switch Component
                     
-                    VStack {
-                        HStack {
-                            Text("Switch 1")
-                                .font(.system(size: 30, weight: .bold, design: .default))
-                            Spacer()
-                            Image(systemName: "lightbulb")
-                                .font(.system(size: 35, weight: .regular, design: .default))
-                                .frame(width: 60, height: 60)
-//                                .background(Color.green)
-//                                .cornerRadius(30)
-                                .foregroundColor(.green)
-                        }
-                        .foregroundColor(Color.green)
-                        .frame(width: 300)
-                        
-                        HStack {
-                            Button(action: {
-                                if (self.toggleSwitch) {
-                                    self.rootRef.child("Switch_1/Switch_status").setValue("OFF")
-                                    self.toggleSwitch = false
-                                } else {
-                                    self.rootRef.child("Switch_1/Switch_status").setValue("On")
-                                    self.toggleSwitch = true
+                    ScrollView {
+                        // Switch Component
+                        ForEach (1 ..< 3) { item in
+                            VStack {
+                                // Name and Logo
+                                HStack {
+                                    Text("Switch 1")
+                                        .font(.system(size: 30, weight: .bold, design: .default))
+                                    Spacer()
+                                    Image(systemName: "lightbulb")
+                                        .font(.system(size: 35, weight: .regular, design: .default))
+                                        .frame(width: 60, height: 60)
+                                        .foregroundColor(.green)
                                 }
-                            })  {  // Inside button
-                                Text(toggleSwitch ? "On" : "Off")
-                                    .padding(.horizontal, 40)
-                                    .padding(.vertical, 20)
-                                    .foregroundColor(toggleSwitch ? Color.white : Color.green)
-                                    .background(toggleSwitch ? Color.green : Color.white)
-                                    .animation(.easeInOut)
-                                    .cornerRadius(20)
-                                    .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 12)
-                                    .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
-                                    //.shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: -1)
-                            }
-                            
-                            Spacer()
-                            
-                            Button(action: {
-                                if (self.toggleML) {
-                                    self.rootRef.child("Switch_1/ML_status").setValue("DISABLED")
-                                    self.toggleML = false
-                                } else {
-                                    self.rootRef.child("Switch_1/ML_status").setValue("enabled")
-                                    self.toggleML = true
+                                .foregroundColor(Color.green)
+                                .frame(width: 300)
+                                
+                                HStack {
+                                    // On Off Button
+                                    Button(action: {
+                                        if (self.toggleSwitch) {
+                                            self.rootRef.child("Switch_1/Switch_status").setValue("OFF")
+                                            self.toggleSwitch = false
+                                        } else {
+                                            self.rootRef.child("Switch_1/Switch_status").setValue("On")
+                                            self.toggleSwitch = true
+                                        }
+                                    })  {  // Inside button
+                                        Text(self.toggleSwitch ? "On" : "Off")
+                                            .padding(.horizontal, 40)
+                                            .padding(.vertical, 20)
+                                            .foregroundColor(self.toggleSwitch ? Color.white : Color.green)
+                                            .background(self.toggleSwitch ? Color.green : Color.white)
+                                            .animation(.easeInOut)
+                                            .cornerRadius(20)
+                                            .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 12)
+                                            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                                        //.shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: -1)
+                                    }
+                                    Spacer()
+                                    // ML Button
+                                    Button(action: {
+                                        if (self.toggleML) {
+                                            self.rootRef.child("Switch_1/ML_status").setValue("DISABLED")
+                                            self.toggleML = false
+                                        } else {
+                                            self.rootRef.child("Switch_1/ML_status").setValue("enabled")
+                                            self.toggleML = true
+                                        }
+                                    }) {
+                                        Text("ML")
+                                            .padding(.horizontal, 40)
+                                            .padding(.vertical, 20)
+                                            .foregroundColor(self.toggleML ? Color.white : Color.green)
+                                            .background(self.toggleML ? Color.green : Color.white)
+                                            .animation(.easeInOut)
+                                            .cornerRadius(20)
+                                            .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 12)
+                                            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                                        //.shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: -1)
+                                    }
                                 }
-                            }) {
-                                Text("ML")
-                                    .padding(.horizontal, 40)
-                                    .padding(.vertical, 20)
-                                    .foregroundColor(toggleML ? Color.white : Color.green)
-                                    .background(toggleML ? Color.green : Color.white)
-                                    .animation(.easeInOut)
-                                    .cornerRadius(20)
-                                    .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 12)
-                                    .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
-                                    //.shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: -1)
+                                .padding(.top, 30)
+                                .padding(.bottom, 10)
+                                .frame(width: 300)
+                                .frame(maxWidth: .infinity)
                             }
-                        }
-                        .padding(.top, 30)
-                        .frame(width: 300)
+                            .frame(width: 300)
+                            .padding(20)
+                            .background(Color.white)
+                            .cornerRadius(20)
+                        } // For Each
                         
-                        
-                    }
-                    .frame(width: 300)
-                    .padding(20)
-                    .background(Color.white)
-                    .cornerRadius(20)
-                    .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 12)
-                    .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
-                    .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: -1)
-                    .offset(y: -150)
-                    
-                    
+                    } // ScrollView Modifier
+                        .frame(maxWidth: .infinity)
+                        .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 12)
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: -1)
+                        .offset(y: screen.height / 4)
                 }
                 .padding(.top, 44)
                 .edgesIgnoringSafeArea(.all)
@@ -169,6 +162,39 @@ struct ContentView: View {
         
     }
 }
+
+struct Switches {
+    let Switch: String
+    let ML_status: String
+    let Switch_status: String
+
+    // Standard init
+    init(Switch: String, ML_status: String, Switch_status: String) {
+        self.Switch = Switch
+        self.ML_status = ML_status
+        self.Switch_status = Switch_status
+    }
+
+    // Init for reading from Database snapshot
+    init(snapshot: DataSnapshot) {
+        let snapshotValue = snapshot.value as! [String: AnyObject]
+        Switch = snapshotValue["Switch"] as! String
+        ML_status = snapshotValue["ML_status"] as! String
+        Switch_status = snapshotValue["Switch_status"] as! String
+    }
+
+    // Func converting model for easier writing to database
+    func toAnyObject() -> Any {
+        return [
+            "Switch": Switch,
+            "ML_status": ML_status,
+            "Switch_status": Switch_status
+        ]
+    }
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

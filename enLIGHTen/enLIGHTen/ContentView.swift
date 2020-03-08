51a00c2914fa2515adecd605a16cbe696739d959
Dominick <d3lee@ucsd.edu>
2020-03-07 19:06:24 -0800

    Sad

--- enLIGHTen/enLIGHTen/ContentView.swift
+++ enLIGHTen/enLIGHTen/ContentView.swift
@@ -1,11 +1,3 @@
-//
-//  ContentView.swift
-//  enLIGHTen
-//
-//  Created by Dominick Lee on 2/6/20.
-//  Copyright © 2020 Dominick Lee. All rights reserved.
-//
-
 import SwiftUI
 import Firebase
 
@@ -18,23 +10,64 @@ struct ContentView: View {
   let userID = Auth.auth().currentUser?.uid
   @State var MLButtonAnimation:[Bool] = []
   @State var switchButtonAnimation:[Bool] = []
-
+  
 
   
+  
+  func getSwitches() {
+    DispatchQueue.main.asyncAfter(deadline: .now()) {
+      for i in 1...3 {
+        var count = 1
+        let switches = `switch`()
+        self.rootRef.child("Switches/Switch_\(i)/").observe(.childAdded) { (snapshot) in
+          //print(snapshot.value! as! String)
+          
+          switch (count) {
+          case 1: switches.ML_status = (snapshot.value! as! String)
+          case 2: switches.Switch_status = (snapshot.value! as! String)
+          case 3: switches.number = (snapshot.value! as! Int)
+          default:
+            print("")
+          }
+          
+          count+=1
+          
+        }
+        if (self.switchesArray.count < 3) {
+          self.switchesArray.append(switches)
+          self.switchButtonAnimation.append((switches.Switch_status == "ON") ? true : false)
+          self.MLButtonAnimation.append((switches.ML_status == "ENABLED") ? true : false)
+        } else {
+          self.switchesArray[i] = switches
+          self.switchButtonAnimation[i] = ((switches.Switch_status == "ON") ? true : false)
+          self.MLButtonAnimation[i] = ((switches.ML_status == "ENABLED") ? true : false)
+        }
+        
+      }
+      
+      print("Getswtiches was called")
+      print(self.switchButtonAnimation)
+      print(self.MLButtonAnimation)
+    }
+  }
+  
+  
   func fetchSwitches() {
+    
     rootRef.child("Switches").observe(.childAdded) { (snapshot) in
         if let dictionary = snapshot.value as? [String : AnyObject] {
           let switches = `switch`()
-          
+
           switches.ML_status = (dictionary["ML_status"] as! String)
           switches.Switch_status = (dictionary["Switch_status"] as! String)
-          self.switchButtonAnimation.append((switches.Switch_status == "On") ? true : false)
-          self.MLButtonAnimation.append((switches.ML_status == "On") ? true : false)
-          
+          self.switchButtonAnimation.append((switches.Switch_status == "ON") ? true : false)
+          self.MLButtonAnimation.append((switches.ML_status == "ON") ? true : false)
+
           //print(switches.ML_status! , switches.Switch_status!)
           self.switchesArray.append(switches)
         }
       }
+    print("fetchswitches was called")
   }
   
   func getUser () {
@@ -111,16 +144,19 @@ struct ContentView: View {
                   HStack {
                     // On Off Button
                     Button(action: {
-                      if (self.switchesArray[index].Switch_status == "On") {
+                      if (self.switchesArray[index].Switch_status == "ON") {
                         self.rootRef.child("Switches/Switch_\(index + 1)/Switch_status").setValue("OFF")
+                        self.rootRef.child("Switches/Switch_\(index + 1)/ML_status").setValue("DISABLED")
                         //self.switchesArray[index].toggleSwitch = false
                         self.switchesArray[index].Switch_status = "OFF"
-                        
+                        self.MLButtonAnimation[index] = false
                         self.switchButtonAnimation[index] = false
                       } else {
-                        self.rootRef.child("Switches/Switch_\(index + 1)/Switch_status").setValue("On")
+                        self.rootRef.child("Switches/Switch_\(index + 1)/Switch_status").setValue("ON")
+                        self.rootRef.child("Switches/Switch_\(index + 1)/ML_status").setValue("DISABLED")
                         //self.switchesArray[index].toggleSwitch = true
-                        self.switchesArray[index].Switch_status = "On"
+                        self.switchesArray[index].Switch_status = "ON"
+                        self.MLButtonAnimation[index] = false
                         self.switchButtonAnimation[index] = true
                       }
                     })  {  // Inside button
@@ -139,7 +175,7 @@ struct ContentView: View {
                     Spacer()
                     // ML Button
                     Button(action: {
-                      if (self.switchesArray[index].ML_status == "enabled") {
+                      if (self.switchesArray[index].ML_status == "ENABLED") {
                         self.rootRef.child("Switches/Switch_\(index + 1)/ML_status").setValue("DISABLED")
                         //self.switchesArray[index].toggleML = false
                         self.switchesArray[index].ML_status = "DISABLED"
@@ -147,9 +183,9 @@ struct ContentView: View {
                         
                         self.MLButtonAnimation[index] = false
                       } else {
-                        self.rootRef.child("Switches/Switch_\(index + 1)/ML_status").setValue("enabled")
+                        self.rootRef.child("Switches/Switch_\(index + 1)/ML_status").setValue("ENABLED")
                         //self.switchesArray[index].toggleML = true
-                        self.switchesArray[index].ML_status = "enabled"
+                        self.switchesArray[index].ML_status = "ENABLED"
                         
                         //TODO
                         self.MLButtonAnimation[index] = true
@@ -180,6 +216,7 @@ struct ContentView: View {
                 .cornerRadius(20)
             } // For Each
             
+            
           } // ScrollView Modifier
             .frame(maxWidth: .infinity)
             .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 12)
@@ -187,9 +224,12 @@ struct ContentView: View {
             .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: -1)
             .offset(y: screen.height / 4)
             //.onAppear{self.fetchSwitches()}
-
         }
-        .onAppear{self.fetchSwitches()}
+        .onAppear{
+          self.getSwitches()
+          //self.fetchSwitches()
+        }
+        
         .padding(.top, 44)
         .edgesIgnoringSafeArea(.all)
         
@@ -206,7 +246,7 @@ class `switch`:ObservableObject {
   
   var ML_status: String?
   var Switch_status: String?
-  
+  var number: Int?
 }
 
 
